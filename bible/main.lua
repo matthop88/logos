a52bf706d41bc791578903c78c37da8e40d1f306
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 14:18:38 -0800

    Added beginning of facility to look up files

--- bible/main.lua
+++ bible/main.lua
@@ -1,2 +1,13 @@
+function findScriptureFile(filename)
+    -- if file is in scriptures, find it
+    -- if not, search all subdirectories
+    local directoryItems = love.filesystem.getDirectoryItems("bible/scriptures")
+    for _, v in ipairs(directoryItems) do
+        print(v)
+    end
+end
+
 print("Welcome to Logos!")
+
+findScriptureFile("philippians")
 love.event.quit()
