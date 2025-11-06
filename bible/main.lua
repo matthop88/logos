8c32244e91663f049db33945a1d13b043884e394
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 14:20:37 -0800

    Added code to look up scripture located somewhere in bible/scripture/ subdirectories

--- bible/main.lua
+++ bible/main.lua
@@ -1,13 +1,17 @@
-function findScriptureFile(filename)
-    -- if file is in scriptures, find it
-    -- if not, search all subdirectories
-    local directoryItems = love.filesystem.getDirectoryItems("bible/scriptures")
+function findScriptureFile(filename, directory)
+    local directoryItems = love.filesystem.getDirectoryItems(directory)
     for _, v in ipairs(directoryItems) do
-        print(v)
+        if v == string.lower(filename) .. ".lua" then
+            return directory .. "/" .. string.lower(filename)
+        else
+            local result = findScriptureFile(filename, directory .. "/" .. v)
+            if result ~= nil then return result end
+        end
     end
 end
 
 print("Welcome to Logos!")
 
-findScriptureFile("philippians")
+local scripturePath = findScriptureFile("philippians", "bible/scriptures")
+print(scripturePath)
 love.event.quit()
