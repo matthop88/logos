54497bf2a67360830c751979851bd1c06f0aaf68
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 14:17:27 -0800

    Updated main.lua; first argument is now accessible via __BOOK_NAME

--- main.lua
+++ main.lua
@@ -2,6 +2,11 @@ function love.load(args)
     for _, arg in ipairs(args) do
         print(arg)
     end
+
+    __BOOK_NAME = args[1]
+
+    print()
+
+    require "bible/main"
 end
 
-require "bible/main"
