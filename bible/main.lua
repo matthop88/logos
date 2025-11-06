d33bc5d26f8db2ee0fb6828e7c9e44d72a382039
Matthew Hopkins <matthop88@gmail.com>
2025-11-06 14:33:35 -0800

    Finished extraction of findScriptureFile() from inner main

--- bible/main.lua
+++ bible/main.lua
@@ -1,21 +1,10 @@
 require("bible/util/consoleFunctions")
-
-function findScriptureFile(filename, directory)
-    local directoryItems = love.filesystem.getDirectoryItems(directory)
-    for _, v in ipairs(directoryItems) do
-        if v == string.lower(filename) .. ".lua" then
-            return directory .. "/" .. string.lower(filename)
-        else
-            local result = findScriptureFile(filename, directory .. "/" .. v)
-            if result ~= nil then return result end
-        end
-    end
-end
+require("bible/util/fileHelper")
 
 if __BOOK_NAME == nil then
     printResponse("ERROR", "Please specify a book of the Bible.")
 else
-    local scripturePath = findScriptureFile(__BOOK_NAME, "bible/scriptures")
+    local scripturePath = locateLuaFile(__BOOK_NAME, "bible/scriptures")
     if scripturePath ~= nil then
         printResponse("OK")
     else
