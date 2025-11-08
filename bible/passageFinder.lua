31501e4ec405133775bc7f4ad566ddfbee3ac0ef
Matthew Hopkins <matthop88@gmail.com>
2025-11-07 17:21:37 -0800

    Moved to a nested result architecture

--- bible/passageFinder.lua
+++ bible/passageFinder.lua
@@ -2,13 +2,17 @@ require("bible/util/fileHelper")
 
 return {
 	findPassage = function(self, bookName, passageInfo)
+		local result = {}
+
 		return self:loadBook(bookName)
 	end,
 
 	loadBook = function(self, bookName)
 		local scripturePath = locateLuaFile(bookName, "bible/scriptures")
     	if scripturePath ~= nil then
-    		return require(scripturePath)
+    		return { bookFound = require(scripturePath) }
+    	else
+    		return { bookFound = nil }
     	end
 	end,
 }
