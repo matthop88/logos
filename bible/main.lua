225659f2c3fb0643aaca3b075d6a39073d9e61e7
Matthew Hopkins <matthop88@gmail.com>
2025-11-07 17:00:09 -0800

    Updated main.lua; now can specify a chapter and it responds correctly.

--- bible/main.lua
+++ bible/main.lua
@@ -1,12 +1,31 @@
 require("bible/util/consoleFunctions")
 require("bible/util/fileHelper")
 
+local findChapter = function(scriptureData, chapterNum)
+	for _, c in ipairs(scriptureData.chapters) do
+		if c.chapter == chapterNum then
+			return c
+		end
+	end
+end
+
 if __BOOK_NAME == nil then
     printResponse("ERROR", "Please specify a book of the Bible.")
 else
     local scripturePath = locateLuaFile(__BOOK_NAME, "bible/scriptures")
     if scripturePath ~= nil then
-        printResponse("OK")
+    	local scriptureData = require(scripturePath)
+
+    	if __PASSAGE_INFO == nil then
+        	printResponse("OK")
+        else
+        	local chapterNum = tonumber(__PASSAGE_INFO)
+        	if findChapter(scriptureData, chapterNum) then
+        		printResponse("OK")
+        	else
+        		printResponse("ERROR", "Chapter not found: " .. __BOOK_NAME .. " " .. chapterNum)
+        	end
+        end
     else
         printResponse("ERROR", "Book not found: " .. __BOOK_NAME)
     end
