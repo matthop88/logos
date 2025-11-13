1b054d275abe0e2fc528bba64d48c611e006f4fc
Matthew Hopkins <matthop88@gmail.com>
2025-11-13 12:30:32 -0800

    Updated passageFinder.lua; added comments about input and output data structure

--- bible/passageFinder.lua
+++ bible/passageFinder.lua
@@ -1,5 +1,43 @@
 require("bible/util/fileHelper")
 
+--[[
+
+New architecture:
+passageInfo will have the following structure:
+
+For a single chapter:
+{ start = { chapter = c } }
+
+For a single verse:
+{ start = { chapter = c, verse = v } }
+
+For a range of verses:
+{ start = { chapter = c, verse = v1 }, finish = { chapter = c, verse = v2 } }
+
+Currently, there is no support for a range of verses spanning multiple chapters.
+
+The passageFinder should return the following:
+
+{
+	book = "{name of the book}",
+	verses = {
+		{	chapter = c, verse = v1,
+			"First line",
+			"Second line",
+			"Third line",
+		},
+	},
+	missing = {
+		{	chapter = c, verse = v1 },
+		{	chapter = c, verse = v2 },
+		etc.
+	},
+}
+
+If the book isn't found, returns nil.
+
+]]
+
 return {
 	findPassage = function(self, bookName, passageInfo)
 		local result = {}
