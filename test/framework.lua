29d15a29943e4bddc64cc7c73f6f32acaac3de5e
Matthew Hopkins <matthop88@gmail.com>
2025-11-13 10:22:00 -0800

    Created test/framework.lua

---
+++ test/framework.lua
@@ -0,0 +1,176 @@
+local ASTERISKS     = "**********************************************************************************************************\n"
+
+love.window.setTitle("Testing Suite... Setting Up Tests")
+
+local totals = {
+    testCount      = 0,
+    testsSucceeded = 0,
+    testsFailed    = 0,
+}
+
+local printCaption = function(str)
+    print(str .. "\n" .. string.rep("-", string.len(str)))
+end
+
+local printBanner = function(str)
+    local asteriskStr = string.rep("*", string.len(str) + 12)
+    print(asteriskStr)
+    print("***   " .. str .. "   ***")
+    print(asteriskStr)
+end
+
+local discoverTests = function()
+    local testClasses = {}
+
+    local testFilenames = love.filesystem.getDirectoryItems("test/tests")
+    for _, testFilename in ipairs(testFilenames) do
+        local keyName = string.sub(testFilename, 1, string.len(testFilename) - 4)
+        local filePath = "test/tests/" .. keyName
+
+        if __PARAMS == nil or __PARAMS["runTests"] == nil or __PARAMS["runTests"] == keyName then
+            table.insert(testClasses, require(filePath))
+        end
+    end
+
+    return testClasses
+end
+    
+TESTING = {
+    initTests = function(self, testsClass)
+        self.runnableTests = {
+            testsClass = testsClass,
+            tests  = { },
+            errors = { },
+
+            testCount      = 0,
+            testsSucceeded = 0,
+            testsFailed    = 0,
+
+            add = function(self, testName, testFn)
+                self.tests[testName] = testFn
+                self.testCount = self.testCount + 1
+            end,
+
+            run = function(self)
+                self.testsSucceeded, self.testsFailed = 0, 0
+                self.errors = {}
+
+                for testName, testFn in pairs(self.tests) do
+                    self:runTest(testFn, testName)
+                end
+            end,
+
+            runTest = function(self, testFn, testName)
+                self:runPretest()
+                local testResult = false
+                local status, err = pcall(function() testResult = testFn(self.testsClass) end)
+                if status == true then
+                    if testResult == true then self.testsSucceeded = self.testsSucceeded + 1
+                    else                       self.testsFailed    = self.testsFailed    + 1 end
+                else
+                    table.insert(self.errors, { testName = testName, err = err })
+                    self.testsFailed = self.testsFailed + 1
+                end
+            end,
+
+            runPretest = function(self)
+                if testsClass.before then
+                    testsClass:before()
+                end
+            end,
+        }
+        
+        for testName, fn in pairs(testsClass) do
+            if testName:sub(1, 4) == "test" then
+                self.runnableTests:add(testName, fn)
+            end
+        end
+    end,
+
+    queueTestClasses = function(self, testClasses)
+        self.queuedTestClasses = testClasses
+    end,
+    
+    runAll = function(self)
+        print("\n")
+        printCaption("Running Tests")
+    
+        for _, testsClass in ipairs(self.queuedTestClasses) do
+            self:initTests(testsClass)
+            self:initiateTests(testsClass)
+            self.runnableTests:run()
+            self:showTestingSummary()
+            self:addTestResultsToTotals()
+        end
+        self:showFinalResults()
+        love.event.quit()
+    end,
+
+    initiateTests = function(self, testsClass)
+        self:printTestName(testsClass)
+        
+        if testsClass.beforeAll then
+            testsClass:beforeAll()
+        end
+    end,
+
+    printTestName = function(self, testsClass)
+        printCaption(testsClass:getName())
+    end,
+
+    showTestingSummary = function(self, testsSucceeded)
+        print("\nTests succeeded: " .. self.runnableTests.testsSucceeded .. " out of " .. self.runnableTests.testCount)
+        if self.runnableTests.testsFailed > 0 then
+            print("\n" .. self.runnableTests.testsFailed .. " tests FAILED.")
+        end
+        self:showFailedTestingDetails()
+        print("\n")
+    end,
+
+    showFailedTestingDetails = function(self)
+        if #self.runnableTests.errors > 0 then
+            for _, error in ipairs(self.runnableTests.errors) do
+                print("FAILED => " .. error.testName)
+                print("          WITH ERROR: " .. error.err)
+            end
+        end
+    end,
+
+    addTestResultsToTotals = function(self)
+        totals.testCount      = totals.testCount      + self.runnableTests.testCount
+        totals.testsSucceeded = totals.testsSucceeded + self.runnableTests.testsSucceeded
+        totals.testsFailed    = totals.testsFailed    + self.runnableTests.testsFailed
+    end,
+
+    showFinalResults = function(self)
+        printBanner("Total Tests Succeeded: " .. totals.testsSucceeded .. " out of " .. totals.testCount)
+        print("\n")
+    end,
+
+    assertTrue = function(self, name, expression)
+        if expression == true then
+            print("PASSED => " .. name)
+            return true
+        else
+            print("\n" .. ASTERISKS .. "FAILED => " .. name .. "\n" .. ASTERISKS)
+            return false
+        end
+    end,
+
+    assertEquals = function(self, name, expected, actual)
+        if expected == actual then
+            print("PASSED => " .. name)
+            return true
+        else
+            print("\n" .. ASTERISKS .. "FAILED => " .. name)
+            print("  Expected: ", expected)
+            print("  Actual: ",   actual)
+            print(ASTERISKS)
+            return false
+        end
+    end,
+}
+
+TESTING:queueTestClasses(discoverTests())
+
+require("test/delayTests")
