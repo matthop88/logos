1caf83ed08227320ee18b0b16d1bcdd5e6daed7e
Matthew Hopkins <matthop88@gmail.com>
2025-11-13 10:32:13 -0800

    Updated outer main to allow testing capability

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 function love.load(args)
-    __BOOK_NAME    = args[1]
-    __PASSAGE_INFO = args[2]
-    require "bible/main"
+    if args[1] == "TEST" then
+        require("test/framework")
+    else
+        __BOOK_NAME    = args[1]
+        __PASSAGE_INFO = args[2]
+        require "bible/main"
+    end
 end
-
