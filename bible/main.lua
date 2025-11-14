02c0419780799eb1fab45a36a965212b63a6f79f
Matthew Hopkins <matthop88@gmail.com>
2025-11-13 17:40:49 -0800

    Updated main.lua to match passage finder's interface

--- bible/main.lua
+++ bible/main.lua
@@ -15,11 +15,16 @@ else
 	passageInfo.book = __BOOK_NAME
 
 	local response = PASSAGE_FINDER:findPassage(passageInfo)
-	if     not response          then printResponse("ERROR", "Book not found: " .. __BOOK_NAME)
-	elseif __PASSAGE_INFO == nil then printResponse("OK")
-    elseif not response.missing  then printResponse("OK")
+	if not response then
+		printResponse("ERROR", "Book not found: " .. __BOOK_NAME)
+	elseif __PASSAGE_INFO == nil then
+        printResponse("OK")
+    elseif response.missing.chapter then
+    	printResponse("ERROR", "Chapter not found: " .. __BOOK_NAME .. " " .. response.missing.chapter)
+    elseif #response.missing > 0 then
+    	printResponse("ERROR", "Verse not found: " .. __BOOK_NAME .. " " .. response.missing[1].chapter .. ":" .. response.missing[1].verse)
     else
-		printResponse("ERROR", "Chapter not found: " .. __BOOK_NAME .. " " .. response.missing.chapter)
+    	printResponse("OK")
     end
 end
 
