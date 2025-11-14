8e3ff0d13dd75868fa1b49b62a63c84e71ac21df
Matthew Hopkins <matthop88@gmail.com>
2025-11-13 17:23:02 -0800

    Implemented some of the logic for passageFinder.lua

--- bible/passageFinder.lua
+++ bible/passageFinder.lua
@@ -6,13 +6,13 @@ New architecture:
 passageInfo will have the following structure:
 
 For a single chapter:
-{ start = { chapter = c } }
+{ book = "{bookName"}, start = { chapter = c } }
 
 For a single verse:
-{ start = { chapter = c, verse = v } }
+{ book = "{bookName"}, start = { chapter = c, verse = v } }
 
 For a range of verses:
-{ start = { chapter = c, verse = v1 }, finish = { chapter = c, verse = v2 } }
+{ book = "{bookName"}, start = { chapter = c, verse = v1 }, finish = { chapter = c, verse = v2 } }
 
 Currently, there is no support for a range of verses spanning multiple chapters.
 
@@ -39,18 +39,38 @@ If the book isn't found, returns nil.
 ]]
 
 return {
-	findPassage = function(self, bookName, passageInfo)
-		local result = {}
+	findPassage = function(self, passageInfo)
+		local scriptures = self:loadBook(passageInfo.book)
+		if scriptures == nil then
+			return nil
+		else
+			local result = {
+				book = passageInfo.book
+			}
 
-		return self:loadBook(bookName)
+			if passageInfo.start then
+				local chapter = self:findChapter(passageInfo.start.chapter, scriptures)
+				if chapter == nil then
+					result.missing = { chapter = passageInfo.start.chapter }
+				end
+			end
+
+			return result
+		end
 	end,
 
 	loadBook = function(self, bookName)
 		local scripturePath = locateLuaFile(bookName, "bible/scriptures")
     	if scripturePath ~= nil then
-    		return { bookFound = require(scripturePath) }
-    	else
-    		return { bookFound = nil }
+    		return require(scripturePath)
     	end
+	end,
+
+	findChapter = function(self, chapter, scriptures)
+		for _, c in ipairs(scriptures.chapters) do
+			if c.chapter == chapter then
+				return c
+			end
+		end
 	end,
 }
