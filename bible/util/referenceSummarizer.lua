22314b63044aad63075f157adb6b06ca047a2ed3
Matthew Hopkins <matthop88@gmail.com>
2025-11-15 12:39:13 -0800

    Create referenceSummarizer.lua

---
+++ bible/util/referenceSummarizer.lua
@@ -0,0 +1,25 @@
+summarizeReference = function(bookName, verses)
+	local ranges = {}
+	for _, v in ipairs(verses) do
+		if #ranges == 0 then
+			table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
+		elseif ranges[#ranges].chapter == v.chapter and v.verse == ranges[#ranges].endVerse + 1 then
+			ranges[#ranges].endVerse = v.verse
+		else
+			table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
+		end
+	end
+
+	local refString = ""
+	for _, r in ipairs(ranges) do
+		if refString ~= "" then
+			refString = refString .. ","
+		end
+		refString = refString .. r.chapter .. ":" .. r.verse
+		if r.verse ~= r.endVerse then
+			refString = refString .. "-" .. r.endVerse
+		end
+	end
+
+	return bookName .. " " .. refString
+end
