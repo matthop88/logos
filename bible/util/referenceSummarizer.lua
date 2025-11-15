f71828f59b6277987c65f3324e163b943284b253
Matthew Hopkins <matthop88@gmail.com>
2025-11-15 12:52:00 -0800

    Update referenceSummarizer.lua to be able to produce reference based upon book name alone

--- bible/util/referenceSummarizer.lua
+++ bible/util/referenceSummarizer.lua
@@ -3,25 +3,27 @@ capitalizeBookName = function(bookName)
 end
 
 summarizeReference = function(bookName, verses)
-	local ranges = {}
-	for _, v in ipairs(verses) do
-		if #ranges == 0 then
-			table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
-		elseif ranges[#ranges].chapter == v.chapter and v.verse == ranges[#ranges].endVerse + 1 then
-			ranges[#ranges].endVerse = v.verse
-		else
-			table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
-		end
-	end
-
 	local refString = ""
-	for _, r in ipairs(ranges) do
-		if refString ~= "" then
-			refString = refString .. ","
+	if verses ~= nil then
+		local ranges = {}
+		for _, v in ipairs(verses) do
+			if #ranges == 0 then
+				table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
+			elseif ranges[#ranges].chapter == v.chapter and v.verse == ranges[#ranges].endVerse + 1 then
+				ranges[#ranges].endVerse = v.verse
+			else
+				table.insert(ranges, { chapter = v.chapter, verse = v.verse, endVerse = v.verse })
+			end
 		end
-		refString = refString .. r.chapter .. ":" .. r.verse
-		if r.verse ~= r.endVerse then
-			refString = refString .. "-" .. r.endVerse
+
+		for _, r in ipairs(ranges) do
+			if refString ~= "" then
+				refString = refString .. ","
+			end
+			refString = refString .. r.chapter .. ":" .. r.verse
+			if r.verse ~= r.endVerse then
+				refString = refString .. "-" .. r.endVerse
+			end
 		end
 	end
 
