1028d36985e0ae7c01d9e1880c292fa919dccc67
Matthew Hopkins <matthop88@gmail.com>
2025-11-15 13:20:38 -0800

    Employ captions in inner main

--- bible/main.lua
+++ bible/main.lua
@@ -1,6 +1,7 @@
 require("bible/util/consoleFunctions")
 require("bible/util/referenceParser")
 require("bible/util/referenceSummarizer")
+local STRING_UTIL = require("bible/util/stringUtil")
 
 local PASSAGE_FINDER = require("bible/passageFinder")
 
@@ -19,14 +20,14 @@ else
 	if not response then
 		printResponse("ERROR", "Book not found: " .. summarizeReference(__BOOK_NAME))
 	elseif __PASSAGE_INFO == nil then
-        print(summarizeReference(__BOOK_NAME))
+        print(STRING_UTIL:caption(summarizeReference(__BOOK_NAME)))
     elseif response.missing.chapter then
     	printResponse("ERROR", "Chapter not found: " .. summarizeReference(__BOOK_NAME, response.missing))
     elseif #response.missing > 0 then
     	local missingVersesSummary = summarizeReference(__BOOK_NAME, response.missing)
     	printResponse("ERROR", "Verse not found: " .. missingVersesSummary)
     else
-    	print(summarizeReference(__BOOK_NAME, response.verses))
+    	print(STRING_UTIL:caption(summarizeReference(__BOOK_NAME, response.verses)))
     end
 end
 
