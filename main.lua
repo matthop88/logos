ec525819fc875e54b3a08c293ff18353c782c872
Matthew Hopkins <matthop88@gmail.com>
2025-11-20 14:42:19 -0800

    Simplified outer main

--- main.lua
+++ main.lua
@@ -8,8 +8,6 @@ function love.load(args)
     if args[1] == "TEST" then
         require("test/framework")
     else
-        __BOOK_NAME    = args[1]
-        __PASSAGE_INFO = args[2]
         parseArgs(args)
         require "bible/main"
     end
