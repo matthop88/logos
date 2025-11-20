fc4d55ffb110e67f235772d763cece4a90189fcd
Matthew Hopkins <matthop88@gmail.com>
2025-11-20 11:53:43 -0800

    Rewrote commandLineParser in object form

--- commandLineParser.lua
+++ commandLineParser.lua
@@ -1,22 +1,22 @@
-function startsWith(str, header)
-	return string.sub(str, 1, #header) == header
-end
+local STRING_UTIL = require("bible/util/stringUtil")
 
-function inSchema(arg, schema)
-	if startsWith(arg, "-") then
-		for k, v in pairs(schema.COMMANDS) do
-			if "--" .. k == arg or "-" .. v.shortcut == arg then
-				return true
+return {
+	inSchema = function(self, arg, schema)
+		if STRING_UTIL:startsWith(arg, "-") then
+			for k, v in pairs(schema.COMMANDS) do
+				if "--" .. k == arg or "-" .. v.shortcut == arg then
+					return true
+				end
 			end
 		end
-	end
-	return false
-end
+		return false
+	end,
 
-function parseCommandLine(args, schema)
-	for n, arg in ipairs(args) do
-		if inSchema(arg, schema) then
-			print("Arg #" .. n .. " in schema: " .. arg)
+	parseCommandLine = function(self, args, schema)
+		for n, arg in ipairs(args) do
+			if self:inSchema(arg, schema) then
+				print("Arg #" .. n .. " in schema: " .. arg)
+			end
 		end
-	end
-end
+	end,
+}
