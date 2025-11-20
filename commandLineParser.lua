ec64f536629bbe684bf4e0ae309ee7b8e9e286f1
Matthew Hopkins <matthop88@gmail.com>
2025-11-20 12:07:04 -0800

    Updated commandLineParser.lua to populate commands from schema and args

--- commandLineParser.lua
+++ commandLineParser.lua
@@ -1,22 +1,29 @@
 local STRING_UTIL = require("bible/util/stringUtil")
 
 return {
-	inSchema = function(self, arg, schema)
+	getSchemaCommand = function(self, arg, schema)
 		if STRING_UTIL:startsWith(arg, "-") then
 			for k, v in pairs(schema.COMMANDS) do
 				if "--" .. k == arg or "-" .. v.shortcut == arg then
-					return true
+					return k
 				end
 			end
 		end
-		return false
+		return nil
 	end,
 
 	parseCommandLine = function(self, args, schema)
+		local expectingValueFor = nil
+		local commands = {}
 		for n, arg in ipairs(args) do
-			if self:inSchema(arg, schema) then
-				print("Arg #" .. n .. " in schema: " .. arg)
-			end
+			local schemaCommand = self:getSchemaCommand(arg, schema)
+			if     schemaCommand ~= nil then commands[schemaCommand]     = true
+			elseif expectingValueFor    then commands[expectingValueFor] = arg  end
+			
+			expectingValueFor = schemaCommand
+		end
+		for k, v in pairs(commands) do
+			print(k .. ": ", v)
 		end
 	end,
 }
