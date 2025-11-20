bc86a38d817a7a43711322e92461dd7a803a96be
Matthew Hopkins <matthop88@gmail.com>
2025-11-20 11:45:22 -0800

    Updated main.lua; temporarily added in global params to reduce breakage

--- main.lua
+++ main.lua
@@ -9,6 +9,8 @@ function love.load(args)
     if args[1] == "TEST" then
         require("test/framework")
     else
+        __BOOK_NAME    = args[1]
+        __PASSAGE_INFO = args[2]
         parseArgs(args)
         require "bible/main"
     end
