2f32db35726f4daf229ae217aac0668bb8cd3e70
Matthew Hopkins <matthop88@gmail.com>
2025-11-21 14:36:24 -0800

    Created filter.lua

---
+++ bible/util/filter.lua
@@ -0,0 +1,40 @@
+local STRING_UTIL = require("bible/util/stringUtil")
+
+local replaceWithDashes = function(str)
+    local newStr = ""
+    for i = 1, string.len(str) do
+    	newStr = newStr .. "-"
+    end
+    return newStr
+end
+
+return {
+	__MIN_LETTER_FILTER = {
+		filterMinLetters = function(self, phrase, minLetters)
+			return self:assemble(self:tokenize(phrase, minLetters))
+		end,
+
+		tokenize = function(self, phrase, minLetters)
+			local tokens = STRING_UTIL:split(phrase)
+			local newTokens = {}
+			for _, t in ipairs(tokens) do
+				table.insert(newTokens, self:transform(t, minLetters))
+		    end
+	        return newTokens
+	    end,
+
+	    transform = function(self, token, minLetters)
+	    	if string.len(token) <= minLetters then return replaceWithDashes(token)
+	    	else                                           return token         end
+	    end,
+
+	    assemble = function(self, tokens)
+	    	return STRING_UTIL:join(tokens)
+	    end,
+	},
+
+	filterMinLetters = function(self, phrase, minLetters)
+		return self.__MIN_LETTER_FILTER:filterMinLetters(phrase, minLetters)
+	end,
+    	
+}
