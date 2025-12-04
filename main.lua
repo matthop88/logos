4b5ac60bc2c82f52b3742e98a537fc8d7b9b9c7a
Matthew Hopkins <matthop88@gmail.com>
2025-12-04 12:51:57 -0800

    Updated outer main to send command and args to memory App

--- main.lua
+++ main.lua
@@ -6,7 +6,13 @@ end
 
 function love.load(args)
     if     args[1] == "TEST" then require("test/framework")
-    elseif args[1] == "MEM"  then require("toolbox/memory/memoryApp")
+    elseif args[1] == "MEM"  then 
+        __COMMAND = args[2]
+        __ARGS    = {}
+        for n, arg in ipairs(args) do
+            if n > 2 then table.insert(__ARGS, arg) end
+        end
+        require("toolbox/memory/memoryApp")
     else
         parseArgs(args)
         require "bible/main"
