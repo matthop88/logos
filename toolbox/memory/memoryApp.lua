72812bcffb3aaaa52e946a13232ab7c99669220f
Matthew Hopkins <matthop88@gmail.com>
2025-12-04 12:53:14 -0800

    Updated memoryApp.lua to print out command and args

--- toolbox/memory/memoryApp.lua
+++ toolbox/memory/memoryApp.lua
@@ -1,3 +1,15 @@
-print("Welcome to the Bible Memory Application!")
+print("\nWelcome to the Bible Memory Application!")
+
+print("\nCOMMAND:", __COMMAND)
+__ARGS = __ARGS or {}
+
+local argList = ""
+
+for _, arg in ipairs(__ARGS) do
+	if string.len(argList) > 0 then argList = argList .. ", " end
+	argList = argList .. arg
+end
+
+print("ARGS: " .. argList .. "\n")
 
 love.event.quit()
