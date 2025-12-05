6a4fd4960cbc58431620153a292f087c80080c37
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 11:16:51 -0800

    Vastly simplified memoryApp.lua by extracting logic to commandParser.lua

--- toolbox/memory/memoryApp.lua
+++ toolbox/memory/memoryApp.lua
@@ -1,30 +1,15 @@
-__COMMAND = __COMMAND or ""
-__ARGS    = __ARGS    or {}
+local CMD_PARSER = require("toolbox/memory/commandParser")
 
-local CMD_VALIDATOR = require("toolbox/memory/commandValidator")
-local ARG_PARSER    = require("toolbox/memory/argParser")
+local result     = CMD_PARSER:parse(__COMMAND or "", __ARGS or {})
 
-local cmdResult  = CMD_VALIDATOR:validate(__COMMAND)
-
-if cmdResult:isError() then
-	cmdResult:print()
+if result:isError() then
+	result:print()
 else
-	local argsResult = ARG_PARSER:parse(__ARGS)
-	if argsResult:isError() then 
-		argsResult:print()
-	else
-		local scripture, filter = argsResult:getData().scripture, argsResult:getData().filter
+	local scripture, filter = result:getData().scripture, result:getData().filter
 		
-		if __COMMAND == "STORE" and filter == nil then
-			require("toolbox/memory/result"):error("No filter specified!"):print()
-		else
-			print("SCRIPTURE: " .. scripture)
-			if filter ~= nil then
-				print("FILTER:    " .. filter)
-			end
-			print()
-		end
-	end
+	print("SCRIPTURE: " .. scripture)
+	if filter ~= nil then print("FILTER:    " .. filter) end
+	print()
 end
 
 love.event.quit()
