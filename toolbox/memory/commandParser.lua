b547413c88d2ef20346cc5a9df4c3a326ec15916
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 11:23:24 -0800

    Updated commandParser.lua to pass command validation of filter to commanValidator.lua

--- toolbox/memory/commandParser.lua
+++ toolbox/memory/commandParser.lua
@@ -5,21 +5,17 @@ local RESULT        = require("toolbox/memory/result")
 
 return {
 	parse = function(self, command, args)
-		local cmdResult  = CMD_VALIDATOR:validate(command)
 		local argsResult = ARG_PARSER:parse(args)
-		
-		if     cmdResult:isError()  then return cmdResult
-		elseif argsResult:isError() then return argsResult
-		else                             return self:generateFullResult(cmdResult, argsResult)
+		if   argsResult:isError() then return argsResult
+		else
+			local scripture, filter = argsResult:getData().scripture, argsResult:getData().filter
+			local cmdResult  = CMD_VALIDATOR:validate(command, scripture, filter)
+			if cmdResult:isError() then return cmdResult
+			else						return self:generateFullResult(cmdResult, scripture, filter) end
 		end
 	end,
 
-	generateFullResult = function(self, cmdResult, argsResult)
-		local scripture, filter = argsResult:getData().scripture, argsResult:getData().filter
-		if command == "STORE" and filter == nil then
-			return RESULT:error("No filter specified!")
-		else
-			return RESULT:success { fn = cmdResult:getData().fn, scripture = scripture, filter = filter }
-		end
+	generateFullResult = function(self, cmdResult, scripture, filter)
+		return RESULT:success { fn = cmdResult:getData().fn, scripture = scripture, filter = filter }
 	end,
 }
