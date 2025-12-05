ccb5cb9efd25902798d3ae208378a6a986091a0c
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 11:27:27 -0800

    Updated commandValidator.lua to validate store with filter

--- toolbox/memory/commandValidator.lua
+++ toolbox/memory/commandValidator.lua
@@ -7,12 +7,19 @@ return {
 		"RECALL",
 	},
 
-	validate = function(self, command)
+	validate = function(self, command, scripture, filter)
+		local validationResult = self:validateCommandWord(command)
+		if     validationResult:isError()           then return validationResult
+		elseif command == "STORE" and filter == nil then return RESULT:error("No filter specified!")
+		else                                             return RESULT:success()                end
+	end,
+
+	validateCommandWord = function(self, commandWord)
 		for _, cmd in ipairs(self.vocab) do
-			if string.upper(command) == cmd then return RESULT:success({ fn = nil }) end
+			if string.upper(commandWord) == cmd then return RESULT:success({ fn = nil }) end
 		end
 
-		return self:getBadCommandError(command)
+		return self:getBadCommandError(commandWord)
 	end,
 
 	getBadCommandError = function(self, command)
