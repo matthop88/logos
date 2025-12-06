c4887d0732c348125bdd5564a7098ef1fc29763f
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 17:32:58 -0800

    Updated memoryApp.lua to load book, parse passage

--- toolbox/memory/memoryApp.lua
+++ toolbox/memory/memoryApp.lua
@@ -1,4 +1,6 @@
-local CMD_PARSER = require("toolbox/memory/commandParser")
+local CMD_PARSER     = require("toolbox/memory/parser/commandParser")
+local BOOK_LOADER    = require("toolbox/memory/lookup/bookLoader")
+local PASSAGE_PARSER = require("toolbox/memory/parser/passageParser")
 
 local result     = CMD_PARSER:parse(__COMMAND or "", __ARGS or {})
 
@@ -7,9 +9,34 @@ if result:isError() then
 else
 	local scripture, filter = result:getData().scripture, result:getData().filter
 		
-	print("SCRIPTURE: " .. scripture)
+	print("SCRIPTURE: " .. scripture.label)
+	print("BOOK:      " .. scripture.book)
+	print("PASSAGE:   " .. scripture.passage)
+
 	if filter ~= nil then print("FILTER:    " .. filter) end
 	print()
+
+	local result = BOOK_LOADER:load(scripture.book)
+	if result:isError() then
+		result:print()
+	else
+		print("Successfully loaded book: " .. scripture.book)
+	end
+
+	local parsedPassage = PASSAGE_PARSER:parse(scripture.passage)
+	print("PARSED PASSAGE:")
+	print("  Start:")
+	print("    Chapter: " .. parsedPassage.start.chapter)
+	if parsedPassage.start.verse then
+		print("    Verse: " .. parsedPassage.start.verse)
+	end
+	if parsedPassage.finish then
+		print("  Finish:")
+		print("    Chapter: " .. parsedPassage.finish.chapter)
+		if parsedPassage.finish.verse then
+			print("    Verse: " .. parsedPassage.finish.verse)
+		end
+	end
 end
 
 love.event.quit()
