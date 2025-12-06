252e5fede3d5ec4049008975c292efb3ce3be126
Matthew Hopkins <matthop88@gmail.com>
2025-12-05 17:31:15 -0800

    Create bookNavigator.lua

---
+++ toolbox/memory/lookup/bookNavigator.lua
@@ -0,0 +1,30 @@
+local BOOK_LOADER = require("toolbox/memory/lookup/bookLoader")
+local RESULT      = require("toolbox/memory/result")
+
+local createNavigator = function(bookData)
+	return {
+		data = bookData,
+
+		getName     = function(self) return self.data.book    end,
+		getVersion  = function(self) return self.data.version end,
+
+    	findChapter = function(self, chapterNumber, scriptures)
+			for _, c in ipairs(self.data.chapters) do
+				if c.chapter == chapterNumber then
+					return c
+				end
+			end
+		end,
+	}
+end
+
+return {
+	create = function(self, bookName)
+		local result = BOOK_LOADER:load(bookName)
+		if result:isError() then return result
+		else                     
+			local bookData = result:getData()
+			return RESULT:success(createNavigator(bookData))
+		end
+	end,
+}
