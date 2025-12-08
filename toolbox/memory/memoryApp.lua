1c989698052a486f21af15e00eac3cb9e59edd15
Matthew Hopkins <matthop88@gmail.com>
2025-12-08 12:42:26 -0800

    Updated memoryApp.lua to use bookNavigator.lua

--- toolbox/memory/memoryApp.lua
+++ toolbox/memory/memoryApp.lua
@@ -1,6 +1,6 @@
 local CMD_PARSER     = require("toolbox/memory/parser/commandParser")
-local BOOK_LOADER    = require("toolbox/memory/lookup/bookLoader")
 local PASSAGE_PARSER = require("toolbox/memory/parser/passageParser")
+local BOOK_NAVIGATOR = require("toolbox/memory/lookup/bookNavigator")
 
 local result     = CMD_PARSER:parse(__COMMAND or "", __ARGS or {})
 
@@ -16,13 +16,6 @@ else
 	if filter ~= nil then print("FILTER:    " .. filter) end
 	print()
 
-	local result = BOOK_LOADER:load(scripture.book)
-	if result:isError() then
-		result:print()
-	else
-		print("Successfully loaded book: " .. scripture.book)
-	end
-
 	local parsedPassage = PASSAGE_PARSER:parse(scripture.passage)
 	print("PARSED PASSAGE:")
 	print("  Start:")
@@ -37,6 +30,22 @@ else
 			print("    Verse: " .. parsedPassage.finish.verse)
 		end
 	end
+
+	local result = BOOK_NAVIGATOR:create(scripture.book)
+	if result:isError() then
+		result:print()
+	else
+		print("Successfully loaded book: " .. scripture.book)
+		local navigator = result:getData()
+		local chapterResult = navigator:findChapter(parsedPassage.start.chapter)
+		if chapterResult:isError() then
+			chapterResult:print()
+		else
+			print("Successfully found chapter " .. parsedPassage.start.chapter)
+		end
+	end
+
+	
 end
 
 love.event.quit()
