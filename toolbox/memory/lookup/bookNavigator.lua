91080c7f7f03c08af43b051b60b4e04cc4489f03
Matthew Hopkins <matthop88@gmail.com>
2025-12-10 11:59:59 -0800

    Updated bookNavigator.lua to chain results properly

--- toolbox/memory/lookup/bookNavigator.lua
+++ toolbox/memory/lookup/bookNavigator.lua
@@ -1,10 +1,40 @@
 local BOOK_LOADER = require("toolbox/memory/lookup/bookLoader")
 local RESULT      = require("toolbox/memory/result")
 
+local enhanceWithVerseMethods = function(result)
+	if not result:isError() then
+		result.display = function(self)
+			for _, line in ipairs(self) do
+				print(line)
+			end
+		end
+	end
+
+	return result
+end
+
+local findVerse = function(self, verseNumber)
+	local result
+	for _, v in ipairs(self.verses) do
+		if v.verse == verseNumber then
+			result = RESULT:success(v)
+		end
+	end
+	result = result or RESULT:error("Verse " .. verseNumber .. " not found.")
+
+	return enhanceWithVerseMethods(result)
+end
+
+local enhanceWithChapterMethods = function(result)
+	if result:isError() then result.findVerse = function(self) return self end
+	else                     result.findVerse = findVerse                  end
+	return result
+end
+
 local enhanceWithNavigatorMethods = function(result)
 	result.getName     = function(self) return self end
 	result.getVersion  = function(self) return self end
-	result.findChapter = function(self, a, b) return self end
+	result.findChapter = function(self, a, b) return enhanceWithChapterMethods(self) end
 
 	return result
 end
@@ -17,10 +47,13 @@ local createNavigator = function(bookData)
 		getVersion  = function(self) return self.data.version end,
 
     	findChapter = function(self, chapterNumber, scriptures)
+			local result
 			for _, c in ipairs(self.data.chapters) do
-				if c.chapter == chapterNumber then return RESULT:success(c) end
+				if c.chapter == chapterNumber then result = RESULT:success(c) end
 			end
-			return RESULT:error("Chapter not found in " .. self:getName() .. ": " .. chapterNumber)
+			result = result or RESULT:error("Chapter not found in " .. self:getName() .. ": " .. chapterNumber)
+
+			return enhanceWithChapterMethods(result)
 		end,
 	}
 end
